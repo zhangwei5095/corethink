ebef0036df1a86ea66a0154be63d15ae19841ff1
JiangRuYi <598821125@qq.com>
2015-08-16 02:02:17 +0800

    新增：SendMessage钩子预留以便于发送消息途径的扩展

--- Application/Common/Model/UserMessageModel.class.php
+++ Application/Common/Model/UserMessageModel.class.php
@@ -51,13 +51,14 @@ class UserMessageModel extends Model{
      * @author jry <598821125@qq.com>
      */
     public function sendMessage($data){
-        $data['title']    = $data['title']; //消息标题
-        $data['content']  = $data['content'] ? : ''; //消息内容
-        $data['to_uid']   = $data['to_uid']; //消息收信人ID
-        $data['type']     = $data['type'] ? : 0; //消息类型
-        $data['from_uid'] = $data['from_uid'] ? : 0; //消息发信人
-        $result = $this->create($data);
+        $msg_data['title']    = $data['title']; //消息标题
+        $msg_data['content']  = $data['content'] ? : ''; //消息内容
+        $msg_data['to_uid']   = $data['to_uid']; //消息收信人ID
+        $msg_data['type']     = $data['type'] ? : 0; //消息类型
+        $msg_data['from_uid'] = $data['from_uid'] ? : 0; //消息发信人
+        $result = $this->create($msg_data);
         if($result){
+            hook('SendMessage', $msg_data); //发送消息钩子，用于消息发送途径的扩展
             return $this->add($result);
         }
     }
